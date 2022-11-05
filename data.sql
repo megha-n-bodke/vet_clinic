d85c2e1ffc9584eed08a7ba7fdebd83673e87320
Megha Bodke <megha.n.bodke@gmail.com>
2022-11-05 14:46:11 +0530

    Work on joins

--- data.sql
+++ data.sql
@@ -15,3 +15,30 @@ INSERT INTO animals (id,name,date_of_birth,escape_attempts,neutered,weight_kg) V
 (9,'Boarmon', '2005-06-07', 7, true, 20.4),
 (10,'Blossom', '1998-10-13', 3, true, 17.0),
 (11,'Ditto', '2022-05-14', 4, true, 22.0);
+
+
+Insert the following data into the species table:
+Pokemon
+Digimon
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+Modify your inserted animals so it includes the species_id value:
+If the name ends in "mon" it will be Digimon
+All other animals are Pokemon
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+Modify your inserted animals to include owner information (owner_id):
+Sam Smith owns Agumon.
+ UPDATE animals set owner_id = 1 WHERE name = 'Agumon';
+Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE animals set owner_id = 2 WHERE name in ('Gabumon','Pikachu');
+Bob owns Devimon and Plantmon.
+UPDATE animals set owner_id = 3 WHERE name in ('Devimon','Plantmon');
+Melody Pond owns Charmander, Squirtle, and Blossom.
+UPDATE animals set owner_id = 4 WHERE name in ('Charmander','Squirtle','Blossom');
+Dean Winchester owns Angemon and Boarmon.
+UPDATE animals set owner_id = 5 WHERE name in ('Angemon','Boarmon');
+
+
+
--- schema.sql
+++ schema.sql
@@ -12,3 +12,38 @@ CREATE TABLE animals (
 
 Add a column species of type string to your animals table. Modify your schema.sql file.
 AFTER TABLE animals ADD COLUMN species varchar(100);
+
+
+/* Primary and FOREIGN key */
+Create a table named owners with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+full_name: string
+age: integer
+
+Create table owners (
+id serial PRIMARY KEY not null,
+full_name varchar(255) not null,
+age integer not null
+);
+
+ 
+Create a table named species with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+name: string
+
+create table species (id serial PRIMARY KEY not null, name varchar(255) not null);
+
+
+Modify animals table:
+Make sure that id is set as autoincremented PRIMARY KEY
+ALTER  TABLE animals Update id serial PRIMARY KEY not null;
+Remove column species
+ALTER TABLE animals DROP COLUMN species;
+select species from animals;
+Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD COLUMN species_id integer REFERENCES species(id);
+Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id integer REFERENCES owners(id);
+select owner_id from animals;
+
+
